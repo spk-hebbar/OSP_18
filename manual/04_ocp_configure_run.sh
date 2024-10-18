58ba3f8bc89f279d18db25e61cbef7f5d6a68a52
Spoorthi K <spk@redhat.com>
2024-10-18 07:11:17 -0400

    Manual script update to incorporate osp_trunk and osp_external nm state
    Signed-off-by: Spoorthi K <spk@redhat.com>

--- manual/04_ocp_configure_run.sh
+++ manual/04_ocp_configure_run.sh
@@ -2,12 +2,13 @@
 
 set -xe
 
-sudo ip link add link eno2 name eno2.1501 type vlan id 1501
-sudo ip link set eno2.1501 up
+#sudo ip link add link eno2 name eno2.1501 type vlan id 1501
+#sudo ip link set eno2.1501 up
 
+sudo chmod -R 777 /home/stack
 sudo chmod -R 777 /home/stack/dev-scripts
 
-make -C $HOME/src/dev-scripts requirements configure
+#make -C $HOME/src/dev-scripts requirements configure
 
 #If you get an error related to SSH public key, copy them manually to the required file
 
@@ -30,6 +31,7 @@ NETWORK_CONFIG=(
 )
 
 # Predefined network interfaces
+export EXTRA_NETWORK_NAMES="osp_trunk osp_external"
 export OSP_TRUNK_IF="enp196s0f1np1"  # osp_trunk
 export OSP_EXTERNAL_IF="eno2.1501"    # osp_external
 
@@ -40,41 +42,24 @@ create_bridge_config() {
     local network_netmask=$3
     local network_if=$4
 
-    sudo tee -a /etc/NetworkManager/system-connections/${network_name}.nmconnection <<EOF
-[connection]
-id=${network_name}
-type=bridge
-interface-name=${network_name}
-[bridge]
-stp=false
-EOF
+    if [ -e "/etc/NetworkManager/system-connections/${network_if}" ]; then
+        sudo rm /etc/NetworkManager/system-connections/${network_if}
+    fi
 
-    # Handle IPv4/IPv6 configuration
-    if [ "$(ipversion $network_ip)" == "6" ]; then
-        sudo tee -a /etc/NetworkManager/system-connections/${network_name}.nmconnection <<EOF
-[ipv4]
-method=disabled
-[ipv6]
-addr-gen-mode=eui64
-address1=${network_ip}/64
-method=manual
+    if [ ${network_if} == ${OSP_EXTERNAL_IF} ]; then
+	sudo tee /etc/NetworkManager/system-connections/${network_if}.nmconnection <<EOF
+[connection]
+id=${network_if}
+type=vlan
+interface-name=${network_if}
+master=${network_name}
+slave-type=bridge
+[vlan]
+parent=eno2
+id=1501
 EOF
     else
-        sudo tee -a /etc/NetworkManager/system-connections/${network_name}.nmconnection <<EOF
-[ipv4]
-address1=${network_ip}/${network_netmask}
-method=manual
-[ipv6]
-addr-gen-mode=eui64
-method=disabled
-EOF
-    fi
-    sudo chmod 600 /etc/NetworkManager/system-connections/${network_name}.nmconnection
-    sudo nmcli con load /etc/NetworkManager/system-connections/${network_name}.nmconnection
-    sudo nmcli con up ${network_name}
-
-    # Configure the network interface
-    sudo tee -a /etc/NetworkManager/system-connections/${network_if}.nmconnection <<EOF
+    sudo tee /etc/NetworkManager/system-connections/${network_if}.nmconnection <<EOF
 [connection]
 id=${network_if}
 type=ethernet
@@ -82,12 +67,41 @@ interface-name=${network_if}
 master=${network_name}
 slave-type=bridge
 EOF
+    fi
+
+    if [ -e "/etc/NetworkManager/system-connections/${network_name}" ]; then
+        sudo rm /etc/NetworkManager/system-connections/${network_name}
+    fi
+
+    sudo tee /etc/NetworkManager/system-connections/${network_name}.nmconnection <<EOF
+[connection]
+id=${network_name}
+type=bridge
+interface-name=${network_name}
+[bridge]
+stp=false
+[ipv4]
+address1=${network_ip}/${network_netmask}
+method=manual
+[ipv6]
+addr-gen-mode=eui64
+method=disabled
+EOF
+
+    sudo chmod 600 /etc/NetworkManager/system-connections/${network_name}.nmconnection
+    sudo nmcli con load /etc/NetworkManager/system-connections/${network_name}.nmconnection
+    sudo nmcli con up ${network_name}
+
+    sudo chmod 600 /etc/NetworkManager/system-connections/${network_if}.nmconnection
+    sudo nmcli con load /etc/NetworkManager/system-connections/${network_if}.nmconnection
+    sudo nmcli con up ${network_if}
+
 }
 
 # Function to apply VLAN trunk mode for osp_trunk
 configure_vlan_trunk() {
-    local network_if=$1
-    sudo tee -a /etc/NetworkManager/system-connections/${network_if}.nmconnection <<EOF
+    local network_name=$1
+    sudo tee -a /etc/NetworkManager/system-connections/${network_name}.nmconnection <<EOF
 [bridge]
 vlan-filtering=true
 [vlan]
@@ -96,25 +110,30 @@ trunk-tags=1502-1505
 EOF
 }
 
-# Check and create osp_trunk connection if it doesn't exist
-if ! nmcli connection show "osp_trunk" > /dev/null 2>&1; then
-    sudo nmcli connection add type bridge con-name "osp_trunk" dev "$OSP_TRUNK_IF" \
-        ip4 "${NETWORK_CONFIG[osp_trunk_ip]}/${NETWORK_CONFIG[osp_trunk_netmask]}" \
-        ipv4.method manual
-    echo "osp_trunk connection created."
-else
-    echo "osp_trunk connection already exists."
-fi
 
-# Check and create osp_external connection if it doesn't exist
-if ! nmcli connection show "osp_external" > /dev/null 2>&1; then
-    sudo nmcli connection add type vlan con-name "osp_external" dev "$OSP_EXTERNAL_IF" \
-        ip4 "${NETWORK_CONFIG[osp_external_ip]}/${NETWORK_CONFIG[osp_external_netmask]}" \
-        ipv4.method manual parent "$OSP_TRUNK_IF" vlan.id 1501
-    echo "osp_external connection created."
-else
-    echo "osp_external connection already exists."
-fi
+ 
+# Check and create osp_trunk connection if it doesn't exist
+#if ! nmcli connection show "osp_trunk" > /dev/null 2>&1; then
+#    sudo nmcli connection add type bridge con-name "osp_trunk" ifname "$OSP_TRUNK_IF" \
+#        ipv4 "${NETWORK_CONFIG[osp_trunk_ip]}/${NETWORK_CONFIG[osp_trunk_netmask]}" \
+#        ipv4.method manual
+#    echo "osp_trunk connection created."
+#else
+#    echo "osp_trunk connection already exists."
+#fi
+#
+## Check and create osp_external connection if it doesn't exist
+#if ! nmcli connection show "osp_external" > /dev/null 2>&1; then
+#    sudo nmcli connection add type vlan con-name "osp_external" ifname "$OSP_EXTERNAL_IF" \
+#        ip4 "${NETWORK_CONFIG[osp_external_ip]}/${NETWORK_CONFIG[osp_external_netmask]}" \
+#        ipv4.method manual dev "$OSP_EXTERNAL_IF" vlan.id 1501
+#    echo "osp_external connection created."
+#else
+#    echo "osp_external connection already exists."
+#fi
+
+#sudo sed -i '/^\[connection\]/a\master=osp_trunk\nslave-type=bridge' /etc/NetworkManager/system-connections/${OSP_TRUNK_IF}.nmconnection
+
 
 # Loop through each network name in EXTRA_NETWORK_NAMES
 for NETWORK_NAME in $EXTRA_NETWORK_NAMES; do
@@ -128,21 +147,17 @@ for NETWORK_NAME in $EXTRA_NETWORK_NAMES; do
         NETWORK_IF="$OSP_EXTERNAL_IF"
     fi
 
-    if [ ! -e /etc/NetworkManager/system-connections/${NETWORK_NAME}.nmconnection ]; then
-        # Create basic bridge config
-        create_bridge_config "$NETWORK_NAME" "$NETWORK_IP" "$NETWORK_NETMASK" "$NETWORK_IF"
-    fi
+    # Create basic bridge config
+    create_bridge_config "$NETWORK_NAME" "$NETWORK_IP" "$NETWORK_NETMASK" "$NETWORK_IF"
 
     # Apply VLAN trunk mode for osp_trunk
     if [ "$NETWORK_NAME" == "osp_trunk" ]; then
-        configure_vlan_trunk "$NETWORK_IF"
+        configure_vlan_trunk "$NETWORK_NAME"
     fi
 
-    sudo nmcli con load /etc/NetworkManager/system-connections/${NETWORK_IF}.nmconnection
-    sudo nmcli con up ${NETWORK_IF}
 done
 
 set +x  # Disable debugging output
 echo "Script completed."
 
-make -C $HOME/src/dev-scripts/ build_installer ironic install_config ocp_run bell
+#make -C $HOME/src/dev-scripts/ build_installer ironic install_config ocp_run bell
